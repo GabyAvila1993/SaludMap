501aeaa66c4fd0ef7cb2400a39c5c3434a0bc16b
Gabriel <avila.gabi@outlook.com>
2025-11-03 03:44:51 -0300

    Integracion completa con ChatBot desplegado y css modiicado

--- Frontend/src/AppWrapper.jsx
+++ Frontend/src/AppWrapper.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { AuthProvider } from './components/Auth/AuthContext';
 import { ChatProvider } from './context/ChatContext';
-import App from './App';
-
+import App from './App.jsx';
 const AppWrapper = () => {
   return (
     <AuthProvider>
